886f8c04434eecc11dd3a2475d2c63a6d4b1878a
luciorim <88190686+luciorim@users.noreply.github.com>
2024-10-15 20:50:29 +0500

    initial

---
+++ src/main/java/com/sdu/peak2peak/Peak2peakApplication.java
@@ -0,0 +1,13 @@
+package com.sdu.peak2peak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peak2peakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Peak2peakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sdu/peak2peak/Peak2peakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdu.peak2peak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peak2peakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
