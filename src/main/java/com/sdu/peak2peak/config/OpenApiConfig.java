f51783dcddb76e60ae0d5798c03990da31b141b7
luciorim <88190686+luciorim@users.noreply.github.com>
2024-10-16 20:00:28 +0500

    add: prod url in openapi

--- src/main/java/com/sdu/peak2peak/config/OpenApiConfig.java
+++ src/main/java/com/sdu/peak2peak/config/OpenApiConfig.java
@@ -29,6 +29,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         description = "Local ENV",
                         url = "http://localhost:8010/"
                 ),
+                @Server(
+                        description = "Prod",
+                        url = "https://peak2peak.onrender.com"
+                )
         },
         security = {
                 @SecurityRequirement(
